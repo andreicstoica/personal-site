55d180128beedc59418f27a42cfce71ac4cc72a9
Andrei Stoica <68513477+andreicstoica@users.noreply.github.com>
2025-07-23 13:59:17 -0400

    speed and structure optimization

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,17 +3,12 @@ import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
 import mdx from "@astrojs/mdx";
 
-
 // https://astro.build/config
 export default defineConfig({
-  output: 'static',
+  output: "static",
   compressHTML: true,
   vite: {
     plugins: [tailwindcss()],
-    resolve: {
-      alias: {},
-    },
   },
-
   integrations: [mdx()],
 });
